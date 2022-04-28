c9407f48028ff55a8dbf0f8484f21a09183882bb
Madan Pratap Yadav <2017137@iiitdmj.ac.in>
2022-04-28 15:06:01 +0530

    third commit

--- ReportAutomation/src/main/java/com/example/awsdemo/RdsData.java
+++ ReportAutomation/src/main/java/com/example/awsdemo/RdsData.java
@@ -1,5 +1,12 @@
 package com.example.awsdemo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
@@ -7,49 +14,19 @@ import com.amazonaws.services.cloudwatch.model.Datapoint;
 import com.amazonaws.services.cloudwatch.model.Dimension;
 import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
 import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.security.Security;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
+public class RdsData {
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
-public class HW1 {
 	
 	static private Double averageCPU=0.0;
     static private Double maxCPU=0.0;
-    static public String insId1="i-06d86faac5c4a9b86";
+    static public String rdsIns="rdsdbinstance-1";
     public static Double getAverageCPU() {
     	return averageCPU;
     }
 
     public static void setAverageCPU(Double averageCPU) {
-    	HW1.averageCPU = averageCPU;
+    	RdsData.averageCPU = averageCPU;
     }
 
     public static Double getMaxCPU() {
@@ -57,7 +34,7 @@ public class HW1 {
     }
 
     public static void setMaxCPU(Double maxCPU) {
-    	HW1.maxCPU = maxCPU;
+    	RdsData.maxCPU = maxCPU;
     }
 	
 public static void main(String args[]) {
@@ -75,7 +52,7 @@ public static void main(String args[]) {
          GetMetricStatisticsRequest statRequest1 = new GetMetricStatisticsRequest();
      
          // set up request message 
-         statRequest1.setNamespace("AWS/EC2"); 
+         statRequest1.setNamespace("AWS/RDS"); 
         
          statRequest1.setPeriod(86400); //period of data(1-day i have set)
         
@@ -94,7 +71,7 @@ public static void main(String args[]) {
          // set time 
      
          SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-         String startDate = "2022-04-20";
+         String startDate = "2022-04-29";
          Date date = null;
 		try {
 			date = sdf.parse(startDate);
@@ -115,11 +92,11 @@ public static void main(String args[]) {
          /* specify an instance */
         
          ArrayList<Dimension> dimensions1 = new ArrayList<Dimension>();
-         dimensions1.add(new Dimension().withName("InstanceId").withValue("i-06d86faac5c4a9b86"));
+         dimensions1.add(new Dimension().withName("DBInstanceIdentifier").withValue("rdsdbinstance-1"));
         
          statRequest1.setDimensions(dimensions1);
        
-         System.out.println("Set up cloud watch for instance: " + insId1);
+         System.out.println("Set up cloud watch for instance: " + rdsIns);
 
        
              GetMetricStatisticsResult statResult1 = cloudWatch.getMetricStatistics(statRequest1);
